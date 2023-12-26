b98c55c539b2bbf6982fd1a95dac5fa325cb5b87
Abeer <afatima@asb.bh>
2023-12-26 11:51:17 +0300

    first commit

---
+++ www/js/index.js
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');
+
+    // Read NDEF formatted NFC Tags
+nfc.addTagDiscoveredListener (
+    function (nfcEvent) {
+        var tag = nfcEvent.tag,
+            ndefMessage = tag.ndefMessage;
+
+        // dump the raw json of the message
+        // note: real code will need to decode
+        // the payload from each record
+        alert(JSON.stringify(nfcEvent));
+        console.log(JSON.stringify(ndefMessage));
+
+        // assuming the first record in the message has
+        // a payload that can be converted to a string.
+        alert(nfc.bytesToString(ndefMessage[0].payload).substring(3));
+    },
+    function () { // success callback
+        alert("Waiting for NDEF tag");
+        console.log("waiting");
+    },
+    function (error) { // error callback
+        alert("Error adding NDEF listener " + JSON.stringify(error));
+        console.log("error");
+    }
+);
+}
